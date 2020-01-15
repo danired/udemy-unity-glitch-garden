d3fce9a746665f2b9622933fb70c49cd019a7081
Dani <daniel.redondo@gmail.com>
2020-01-15 23:20:53 +0100

    Added Fox attacker

---
+++ Assets/Scripts/Gravestone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+
+        Attacker attacker = otherObject.GetComponent<Attacker>();
+        if (attacker)
+        {
+            // ...
+        }
+    }
+}
