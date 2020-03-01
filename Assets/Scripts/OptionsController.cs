3c3ab8821b202f010492b420d5269b8ca811e89c
Dani <daniel.redondo@gmail.com>
2020-03-02 00:09:03 +0100

    Options Screen: Set Volume and save it to PlayerPrefs

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+    [SerializeField] Slider difficultySlider;
+
+    void Start()
+    {
+
+        if (!PlayerPrefsController.HasMasterVolume())
+        {
+            PlayerPrefsController.SetMasterVolume(this.defaultVolume);
+        }
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        } else
+        {
+            Debug.LogWarning("No music player found... did you start from splash screen?");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
