468f019dee6180d7dcaa1ad48a47a6e770b0792a
Dani <daniel.redondo@gmail.com>
2020-03-09 23:36:18 +0100

    Tidy hierarchy when creating Defenders and Projectiles

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -11,10 +11,23 @@ public class Shooter : MonoBehaviour
     AttackerSpawner myLaneSpawner;
     Animator animator;
 
+    GameObject projectileParent;
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
+
     private void Start()
     {
         SetLaneSpawner();
         animator = GetComponent<Animator>();
+        CreateProjectileParent();
+    }
+
+    private void CreateProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
     }
 
     private void SetLaneSpawner()
@@ -56,6 +69,7 @@ public class Shooter : MonoBehaviour
 
     public void Fire()
     {
-        Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+        GameObject newProjectile = Instantiate(projectile, gun.transform.position, gun.transform.rotation) as GameObject;
+        newProjectile.transform.parent = projectileParent.transform;
     }
 }
